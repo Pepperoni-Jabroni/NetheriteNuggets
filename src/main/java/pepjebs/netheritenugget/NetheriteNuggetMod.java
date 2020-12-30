569eea108bc8fda9c01e6979baa664216f52de2d
rohn <nrohn@tx.rr.com>
2020-12-30 15:03:58 -0600

    Added nuggets as a drop chance from Piglin Brutes w/ config

--- src/main/java/pepjebs/netheritenugget/NetheriteNuggetMod.java
+++ src/main/java/pepjebs/netheritenugget/NetheriteNuggetMod.java
@@ -1,8 +1,19 @@
 package pepjebs.netheritenugget;
 
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.loot.BinomialLootTableRange;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.condition.LootConditionTypes;
+import net.minecraft.loot.entry.ItemEntry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import pepjebs.netheritenugget.config.NetheriteNuggetConfig;
 import pepjebs.netheritenugget.item.NetheriteNuggetItems;
 
 public class NetheriteNuggetMod implements ModInitializer {
@@ -13,5 +24,21 @@ public class NetheriteNuggetMod implements ModInitializer {
     @Override
     public void onInitialize(){
         NetheriteNuggetItems.onLoad();
+        AutoConfig.register(NetheriteNuggetConfig.class, JanksonConfigSerializer::new);
+
+        NetheriteNuggetConfig conf = AutoConfig.getConfigHolder(NetheriteNuggetConfig.class).getConfig();
+        if (conf.piglinBruteNetheriteNuggetDropChance > 1.0F) conf.piglinBruteNetheriteNuggetDropChance = 1.0F;
+        LOGGER.info("piglinBruteNetheriteNuggetDropChance: " + 100*conf.piglinBruteNetheriteNuggetDropChance + "%");
+        if (conf.piglinBruteNetheriteNuggetDropChance > 0) {
+            LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+                if (id.equals(EntityType.PIGLIN_BRUTE.getLootTableId())) {
+                    FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                            .rolls(BinomialLootTableRange.create(1, conf.piglinBruteNetheriteNuggetDropChance))
+                            .withEntry(ItemEntry.builder(NetheriteNuggetItems.NETHERITE_NUGGET).build());
+
+                    supplier.withPool(poolBuilder.build());
+                }
+            });
+        }
     }
 }
---
+++ src/main/java/pepjebs/netheritenugget/config/NetheriteNuggetConfig.java
@@ -0,0 +1,14 @@
+package pepjebs.netheritenugget.config;
+
+import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
+import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
+import pepjebs.netheritenugget.NetheriteNuggetMod;
+
+@Config(name = NetheriteNuggetMod.MODID)
+public class NetheriteNuggetConfig implements ConfigData {
+
+    @Comment("The [0-1] (0%-100%) chance of a Piglin Brute dropping a Netherite Nugget on death.")
+    public float piglinBruteNetheriteNuggetDropChance = 0.5F;
+}
