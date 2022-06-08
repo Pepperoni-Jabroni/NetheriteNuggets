c68e8dad733dd4cf5c24c92d509d1d84d4886c2d
Pepperoni-Jabroni <nickrohn18@gmail.com>
2022-06-07 19:40:00 -0700

    1.19

--- src/main/java/pepjebs/netheritenugget/NetheriteNuggetMod.java
+++ src/main/java/pepjebs/netheritenugget/NetheriteNuggetMod.java
@@ -3,9 +3,10 @@ package pepjebs.netheritenugget;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.loot.v2.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.entity.EntityType;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 import org.apache.logging.log4j.LogManager;
@@ -27,13 +28,11 @@ public class NetheriteNuggetMod implements ModInitializer {
         if (conf.piglinBruteNetheriteNuggetDropChance > 1.0F) conf.piglinBruteNetheriteNuggetDropChance = 1.0F;
         if (conf.piglinBruteNetheriteNuggetDropChance < 0.0F) conf.piglinBruteNetheriteNuggetDropChance = 0.0F;
         if (conf.piglinBruteNetheriteNuggetDropChance > 0) {
-            LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+            LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
                 if (id.equals(EntityType.PIGLIN_BRUTE.getLootTableId())) {
-                    FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                    supplier.pool(LootPool.builder()
                             .rolls(BinomialLootNumberProvider.create(1, conf.piglinBruteNetheriteNuggetDropChance))
-                            .withEntry(ItemEntry.builder(NetheriteNuggetItems.NETHERITE_NUGGET).build());
-
-                    supplier.withPool(poolBuilder.build());
+                            .with(ItemEntry.builder(NetheriteNuggetItems.NETHERITE_NUGGET).build()).build());
                 }
             });
         }
